e93c072f4945cbfd843ab249a3c84615d1b1b032
Souheil CHELFOUH <trollfot@gmail.com>
2012-01-20 13:15:49 +0100

    Changed typo in setup

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ setup(name = name,
       classifiers = [
           'Environment :: Web Environment',
           'Intended Audience :: Other Audience',
-          'License :: OSI Approved :: Zope Public License (GPL)',
+          'License :: OSI Approved :: Zope Public License (ZPL)',
           'Operating System :: OS Independent',
           'Programming Language :: Python',
           ],
